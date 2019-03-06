6a945fdf7d22b2861b0d60a37fa0a23762d00f8f
Imashi1 <fabianb175@gmail.com>
2019-03-06 20:40:40 -0300

    Verdulería Update
    Añadido /venta para venta de frutas y verduras.

--- TelegramBot.py
+++ TelegramBot.py
@@ -18,8 +18,11 @@ def minuta(bot,update, pass_chat_data=True):
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="Te da lata ir a revisar los almuerzos por la pagina o presencialmente, yo te puedo ayudar!. Solo escribe /minuta\n\n Creadores: Fabián Bugueño / Kevin Lagos\n\n Puedes checkear el repositorio de github en github.com/Imashi1/MinutaBot")
 
+def venta(bot, update):
+        bot.send_message(chat_id=update.message.chat_id, text="También tenermos frutas y verduras a la venta para hacer de tu experiencia culinaria, la mejor posible.\n\n Frutas\n\n Uva 1Kg $1000\n Tomate 1 Kg $700\n\n Verduras\n\n Cebolla 1Kg $500\n Zanahoria 1Kg $500\n Papas 1 Kg $500\n Pimentón c/u $200\n Choclo c/u $200\n Lechuga c/u $500 ")
 minuta_handler = CommandHandler('minuta',minuta)
 start_handler = CommandHandler('start',start)
+venta_handler = CommandHandler('venta',venta)
 dispatcher = mi_bot_updater.dispatcher
 
 dispatcher.add_handler(start_handler)
