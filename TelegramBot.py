58e222dd32f776aaf8a9ff119c27ada51b6594a0
kevin alexander lagos lavin <klagos@ohiggins.labs.inf.utfsm.cl>
2019-03-06 22:51:14 -0300

    fix: Telegrambot.py not reading venta command

--- TelegramBot.py
+++ TelegramBot.py
@@ -18,7 +18,7 @@ def minuta(bot,update, pass_chat_data=True):
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="Te da lata ir a revisar los almuerzos por la pagina o presencialmente, yo te puedo ayudar!. Solo escribe /minuta\n\n Creadores: Fabián Bugueño / Kevin Lagos\n\n Puedes checkear el repositorio de github en github.com/Imashi1/MinutaBot")
 
-def venta(bot, update):
+def venta(bot, update, pass_chat_data=True):
         bot.send_message(chat_id=update.message.chat_id, text="También tenemos frutas y verduras a la venta para hacer de tu experiencia culinaria, la mejor posible.\n\n Frutas\n\n Uva 1Kg $1000\n Tomate 1 Kg $700\n\n Verduras\n\n Cebolla 1Kg $500\n Zanahoria 1Kg $500\n Papas 1 Kg $500\n Pimentón c/u $200\n Choclo c/u $200\n Lechuga c/u $500 ")
 minuta_handler = CommandHandler('minuta',minuta)
 start_handler = CommandHandler('start',start)
@@ -27,6 +27,7 @@ dispatcher = mi_bot_updater.dispatcher
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(minuta_handler)
+dispatcher.add_handler(venta_handler)
 
 mi_bot_updater.start_polling()
 mi_bot_updater.idle()
